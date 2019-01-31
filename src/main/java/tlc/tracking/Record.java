14a8a48430697c72d283f48ef5c30398a3f680c8
Malah <malah@neuf.fr>
2019-01-31 11:35:36 +0100

    refactor record

--- src/main/java/tlc/tracking/Record.java
+++ src/main/java/tlc/tracking/Record.java
@@ -1,24 +1,19 @@
 package tlc.tracking;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Record {
-    // Needed by Jackson: https://stackoverrun.com/fr/q/9179093#33362329
-    public Record() {}
 
-    public Record(int id, double lat, double lon, String user, long timestamp) {
-        this.id = id;
-        this.lat = lat;
-        this.lon = lon;
-        this.user = user;
-        this.timestamp = timestamp;
-    }
-
-    public String toString() {
-        return "Record(" +this.id+ ", " +this.lat+ ", " +this.lon+ ", " +this.user+ ", " +this.timestamp+")";
-    }
-
-    public int id;
-    public double lat;
-    public double lon;
-    public String user;
-    public long timestamp;
+    private int id;
+    private double lat;
+    private double lon;
+    private String user;
+    private long timestamp;
 }
