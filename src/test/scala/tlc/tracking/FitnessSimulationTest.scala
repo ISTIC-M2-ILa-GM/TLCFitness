303f2a081cbd04f7da21fd04176ae49e8771bf19
Hugo BANNIER <gitlab@penher.bzh>
2019-02-06 13:13:38 +0100

    Configuration du pom pour compiler le scala Ajout d'une classe de test scala

---
+++ src/test/scala/tlc/tracking/FitnessSimulationTest.scala
@@ -0,0 +1,21 @@
+package tlc.tracking
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class FitnessSimulationTest extends Simulation {
+
+  val httpConf = http.baseUrl("http://127.0.0.1:8080")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+
+  val scn = scenario("FitnessSimulation")
+    .exec(http("add")
+      .post("/api/run")
+      .body(StringBody("[{\"id\":9,\"lat\":48.8601,\"lon\":2.3507,\"user\":\"lea\",\"timestamp\":1543775727}]")))
+    .pause(5)
+
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpConf)
+}
+
